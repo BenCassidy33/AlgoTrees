cafec28d09e007c71b61a7a89c390e1f4bc0a9df
BenCassidy33 <ben.d.cassidy@gmail.com>
2023-06-23 00:03:16 -0400

    updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
-use algographs::algographs::binary_tree::actions::*;
-use algographs::algographs::binary_tree::*;
-
 fn main() {
-    let mut graph: BinaryTree<i32> = BinaryTree::create_empty();
-    graph.insert_right(InsertionType::Empty);
-
-    println!("{:?}", graph);
+    println!("Hello, world!");
 }

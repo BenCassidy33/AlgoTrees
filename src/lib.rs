510a0d67d12b570db63cade54ed97d7799782fe7
BenCassidy33 <ben.d.cassidy@gmail.com>
2023-06-23 00:34:15 -0400

    final commit for the night

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ pub mod prelude {
 }
 
 pub mod algographs {
+
     pub mod binary_tree {
 
         #[derive(Default, Debug)]
@@ -131,8 +132,24 @@ pub mod algographs {
                 }
 
                 pub fn get_head(&self) -> &T {
+                    //;)
                     &self.head
                 }
+            }
+        }
+
+        pub mod search {
+            use super::BinaryTree;
+
+            enum Direction {
+                Right(i32),
+                Left(i32),
+            }
+
+            impl<T> BinaryTree<T> {
+                //TODO:
+                //pub fn directions(haystack: BinaryTree<T>, needle: T) -> Option<Vec<Direction>> {}
+                //pub fn bfs(haystack: BinaryTree<T>) -> &T {}
             }
         }
     }

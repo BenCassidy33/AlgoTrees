63b3c7a99ee9830bdd82ee0ef87fec0bb5e678e5
Benjamin Cassidy <ben.d.cassidy@gmail.com>
2023-09-17 12:38:34 -0400

    changed version and removed main function code

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,1 @@
-mod queue;
-use algotrees::queue::queues;
-use algotrees::queue::queues::{Queue, TQueue};
-
-fn main() {
-    let mut queue: queues::Queue<i32> = queues::new();
-    queue.push(1);
-    queue.push(2);
-    queue.push(3);
-    println!("Queue: {:?}", queue);
-    println!("Queue: {:?}", queue.pop());
-    println!("Queue: {:?}", queue);
-
-    let smaller_queue = queue.peek_range_mut(0, 2);
-    println!("Smaller queue: {:?}", smaller_queue);
-    let bad_queue = queue.peek_range_mut(0, 3);
-    println!("Bad queue: {:?}", bad_queue);
-}
+fn main() {}
